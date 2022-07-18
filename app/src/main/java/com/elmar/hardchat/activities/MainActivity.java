fb1785ae0bad49899933b4a5b3c4e7ab65464669
Elmar <elmarismaylov26@gmail.com>
2022-07-18 21:48:02 +0400

    Sign Up Using Firestore Database

--- app/src/main/java/com/elmar/hardchat/activities/MainActivity.java
+++ app/src/main/java/com/elmar/hardchat/activities/MainActivity.java
@@ -1,8 +1,10 @@
-package com.elmar.hardchat;
+package com.elmar.hardchat.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.elmar.hardchat.R;
 
 public class MainActivity extends AppCompatActivity {
 
